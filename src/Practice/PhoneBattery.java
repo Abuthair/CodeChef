0c85ec12fec21372218f0573aab36dc13c0c70c2
Abuthair <Sheikabuthair.pc@gmail.com>
2023-03-07 22:40:04 +0530

    Day 1

--- src/Practice/PhoneBattery.java
+++ src/Practice/PhoneBattery.java
@@ -1,6 +1,26 @@
-package Practice;/**
- * @project CodeChef
+package Practice;
+
+import java.util.Scanner;
+
+/**
  * @author abuthair on 07/03/23.
- *10:35 pm
- */public class PhoneBattery {
+ * 10:35 pm
+ * @project CodeChef
+ */
+public class PhoneBattery {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int testCase = sc.nextInt();
+        while(testCase != 0){
+            int X = sc.nextInt();
+            if(X < 80){
+                System.out.println("No");
+            }
+            else{
+                System.out.println("Yes");
+            }
+
+            testCase--;
+        }
+    }
 }
